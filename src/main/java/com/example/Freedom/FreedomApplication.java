0a17d5b455d7bb8ea7669d35176803a4ed3f35e0
Vij02 <amancha.vijayalaxmi@gmail.com>
2023-12-06 12:03:20 +0530

    first commit

---
+++ src/main/java/com/example/Freedom/FreedomApplication.java
@@ -0,0 +1,14 @@
+package com.example.Freedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreedomApplication.class, args);
+		System.out.println("Hi");
+	}
+
+}
